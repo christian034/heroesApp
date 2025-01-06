b646cd444d113021692ad7a933a68dbb73d24c60
Christian Alvarez <christianaah034@gmail.com>
2025-01-06 16:30:25 -0600

    Cascaron de la app heroesApp

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+//AngularModule
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//authRoutingModule
+import { AuthRoutingModule } from './auth-routing.module';
+//auth pages component
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
